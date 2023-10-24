b94145a7d8ae4c561e954cd7da06d0b80cc39acd
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-10-24 11:27:19 -0300

    Add files via upload

---
+++ projeto-a3/index.php
@@ -0,0 +1,20 @@
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Segurança</title>
+</head>
+<body>
+	<center>
+		<h1>SEGURANÇA NA WEB</h1>
+		<h3>Login</h3>
+		<form id="form-login" action="login.php" method="POST">
+			Login: <input type="text" name="login"><br>
+			Senha: <input type="password" name="senha"><br><br>
+			<input type="submit" name="entrar" value="Entrar">
+		</form>
+	</center>
+</body>
+</html>
