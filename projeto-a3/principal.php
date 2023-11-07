b7e9a3fe3076a8292f344818f7c32f97e06ed6f5
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-07 11:49:51 -0300

    Update principal.php

--- projeto-a3/principal.php
+++ projeto-a3/principal.php
@@ -3,7 +3,27 @@
 session_start();
 
 include('conexao.php');
-
-
+include('funcoes.php');
 
 ?>
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
+<!DOCTYPE html>
+<html lang="pt- BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+		body {
+		background: linear-gradient(-200deg, #87CEFA 10%, rgba(47, 79, 79, 100) 50%) no-repeat; 	
+		}
+	</style>
+    <title></title>
+</head>
+<body>
+    <center>
+       <a href="addusuario.php">
+        Adicionar Usuário</a>
+    </center>
+</body>
+</html>
