692cbbce935fc564cecd0612c6e9260a29e1944d
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-17 19:16:26 -0300

    Update addusuario.php

--- addusuario.php
+++ addusuario.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 include('conexao.php');
@@ -11,65 +12,59 @@ $telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
 $login = isset($_POST['login']) ? $_POST['login'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-$selectcpf =  "SELECT cpf FROM usuario WHERE cpf = '$cpf'";
+$selectcpf = "SELECT cpf FROM usuario WHERE cpf = '$cpf'";
 $querycpf = mysqli_query($conexao, $selectcpf);
 $dadocpf = mysqli_fetch_row($querycpf);
-//------------------------------------------//
-$selectlogin =  "SELECT login FROM login WHERE login = '$login'";
+
+$selectlogin = "SELECT login FROM login WHERE login = '$login'";
 $querylogin = mysqli_query($conexao, $selectlogin);
-$dadologin = mysqli_fetch_row($querylogin); 
+$dadologin = mysqli_fetch_row($querylogin);
 
-if($nome <> NULL) {
-    if (($dadocpf == NULL) && ($dadologin == NULL)) {
-        $insertusuario = "INSERT INTO usuario (nome, cpf, telefone)
-         VALUES 
-        ('$nome', '$cpf', '$telefone')";
+if ($nome <> NULL) {
+	if (($dadocpf == NULL) && ($dadologin == NULL)){
+		$insertusuario = "INSERT INTO usuario (nome, cpf, telefone)
+		VALUES 
+		('$nome', '$cpf', '$telefone')";
+		$queryusuario = mysqli_query($conexao, $insertusuario);
 
-        $queryusuario = mysqli_query($conexao, $insertusuario);
+		$senhacriptografada = criptografar($senha);
 
-        $senha_criptografada = criptografar($senha);
-
-        $insertlogin = "INSERT INTO login (cpf, login, senha, nivel)
-         VALUES
-        ('$cpf', '$login', '$senha_criptografada', 3)";
-
-        $querylogin = mysqli_query($conexao, $insertlogin);
-       
-        echo '<script>alert("Usuário cadastrado com sucesso!");
-        window.location="addusuario.php";
-        </script>';
-    }  
-
+		$insertlogin = "INSERT INTO login (cpf, login, senha, nivel) 
+		VALUES
+		('$cpf', '$login', '$senhacriptografada', 3)";
+		$querylogin = mysqli_query($conexao, $insertlogin);
+		echo '<script>alert("Usuário cadastrado com sucesso");
+				window.location="index.php";
+				</script>';
+	} else {
+		echo '<script>alert("CPF e/ou Login já cadastrados");
+				window.location="addusuario.php";
+				</script>';
+	}
 }
 
 ?>
 
 <link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <!DOCTYPE html>
-<html lang="pt- BR">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-		body {
-            background: rgb(144,144,144);
-background: linear-gradient(14deg, rgba(144,144,144,0.958420868347339) 1%, rgba(119,118,118,1) 5%, rgba(255,255,255,1) 29%, rgba(60,152,171,1) 72%, rgba(0,212,255,1) 100%);
-		}
-	</style>
-    <title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" text="text/css" href="estilo.css">  
+	<title></title>
 </head>
 <body>
-    <center>
-         <h1>Adicionar Usuário</h1>
-        <form   id="form-addusuario" action="#" method= "POST">
-            Nome : <input type="text" name="nome" required><br>
-            CPF  :  <input type="text" name="cpf" required><br>
-            Telefone : <input type="text" name="telefone" required><br>
-           Login : <input type="text" name="login" required><br>
-           Senha : <input type="password" name="senha" required><br><br>
-            <input type="submit" name="enviar" value="Enviar">
-        </form>
-    </center>
+	<center>
+		<h1>Adicionar Usuário</h1>
+		<form id="form-addusuario" action="#" method="POST">
+			Nome: <input type="text" name="nome" required><br>
+			CPF: <input type="text" name="cpf" required><br>
+			Telefone: <input type="text" name="telefone" required><br>
+			Login: <input type="text" name="login" required><br>
+			Senha: <input type="password" name="senha" required><br><br>
+			<input type="submit" name="enviar" value="Enviar"><br><br>
+		</form>
+	</center>
 </body>
 </html>
