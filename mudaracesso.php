3da7f386e90c85e73561cf3f3da83c2a6e98793d
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-17 19:18:39 -0300

    Update mudaracesso.php

--- mudaracesso.php
+++ mudaracesso.php
@@ -5,51 +5,46 @@ session_start();
 include('conexao.php');
 include('validaradmin.php');
 
-
-
-    $select = "SELECT nome, descricao, nivel.id, login.cpf, FROM usuario INNER JOIN login ON usuario.cpf = login.cpf 
-    INNER JOIN nivel ON nivel.id = nivel";
-    $queryselect = mysqli_query($conexao ,$select);
-
+$select = "SELECT nome, descricao, nivel.id, login.cpf FROM usuario 
+			INNER JOIN login ON usuario.cpf = login.cpf
+			INNER JOIN nivel ON nivel.id = nivel";
+$queryselect = mysqli_query($conexao, $select);
 
 ?>
+
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <!DOCTYPE html>
-<html lang="pt- BR">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+        body{
+            background-color:#DEDAD2;
+            font-family:"Lobster";
+            top:50%;
+            left:50%;
+            width: 400px;
+            padding:250px;
+        }
+    </style>
+	<title></title>
 </head>
 <body>
-    <center>
-     <form name="mudatipo" action="mudartipo.php" method="POST">
-        <table border="1px">
-            <tr>
-                <td>Nome</td>
-                <td>Tipo de Usuário</td>
-                <td>Novo Tipo de usuario</td>
-                <td>Alterar</td>
-            </tr>
-            <?php while ($linha = mysqli_fetch_row($queryselect))   { ?>
-            <tr>
-                <td><?php echo $linha[0] ?></td>
-                <td><?php echo $linha[1] ?></td>
-                <td>
-                    <select name="nivel">
-                        <option value="1">Administrador</option>
-                        <option value="2">Gerente</option>
-                        <option value="3">Usuário</option>
-                    </select>    
-                </td>
-                <td>
-                    <input type="submit" name="alterar" value="Alterar">   
-                    <input type="hidden" name="cpf" value="<?php echo $linha[3]?>">
-                </td>
-            </tr>    
-           <?php  } ?>
-        </table>        
-     </form>    
-    </center>
+	<center>
+		<table border="1px">
+			<tr>
+				<td>Nome</td>
+			</tr>
+			<?php
+			while ($linha = mysqli_fetch_row($queryselect)) { ?>
+			<tr>
+				<td><a href="chamausuario.php?cod=<?php echo $linha[3] ?>"><?php echo $linha[0] ?></a></td>
+			</tr>
+		<?php } ?>
+		</table><br>
+        <a href="principal.php"><input type="button" value="Sair"></a>
+		</form>
+	</center>
 </body>
 </html>
