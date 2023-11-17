f7beaabe345ef9f99efcdaf99a8b9d4408586019
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-17 19:17:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-		body {
-			background: rgb(144,144,144);
-background: linear-gradient(14deg, rgba(144,144,144,0.958420868347339) 1%, rgba(119,118,118,1) 5%, rgba(255,255,255,1) 29%, rgba(60,152,171,1) 72%, rgba(0,212,255,1) 100%);	
-		}
-	</style>
+    <link rel="stylesheet" text="text/css" href="estilo.css">  
 	<title>Segurança</title>
 </head>
 <body>
