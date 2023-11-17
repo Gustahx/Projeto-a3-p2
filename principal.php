dce8b48d9fc8c2a63606aada6e263b22ece60daf
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-17 19:18:54 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -3,38 +3,30 @@
 session_start();
 
 include('conexao.php');
-include('funcoes.php');
 include('validarlogin.php');
 
 $nivel = $_SESSION['nivel'];
 
 ?>
-<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
+
 <!DOCTYPE html>
-<html lang="pt- BR">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-		body {
-            background: rgb(144,144,144);
-background: linear-gradient(14deg, rgba(144,144,144,0.958420868347339) 1%, rgba(119,118,118,1) 5%, rgba(255,255,255,1) 29%, rgba(60,152,171,1) 72%, rgba(0,212,255,1) 100%);
-		}
-	</style>
-    <title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" text="text/css" href="estilo.css">  
+	<title></title>
 </head>
 <body>
-    <center>
-        <?php
-        if ($nivel < 3 ) { ?>
-       <a href="addusuario.php">
-        Adicionar Usuário</a> |
-        <?php }
-        if($nivel == 1) { ?>
-         <a href="mudaracesso.php">Mudar Acesso</a>
-         <?php } ?><br>
-        <a href="logout.php">Sair</a>
-    </center>
+	<center>
+		<?php
+		if ($nivel < 3) { ?>
+			 <a href="addusuario.php"><input type="button" name="Adicionar Usuário" value="Adicionar Usuário"></a><br><br>
+            <?php }
+			if ($nivel == 1) { ?>
+                <a href="mudaracesso.php"><input type="button" name="Mudar Acesso" value="Mudar Acesso"></a><br><br>
+		<?php } ?>
+        <a href="logout.php"><input type="button" name="Sair" value="Sair"></a>
+	</center>
 </body>
 </html>
