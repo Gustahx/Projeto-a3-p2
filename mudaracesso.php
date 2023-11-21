cbd7dded696c0ae686d1c717e30c592963b070d0
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-21 09:44:20 -0300

    Update mudaracesso.php

--- mudaracesso.php
+++ mudaracesso.php
@@ -12,19 +12,18 @@ $queryselect = mysqli_query($conexao, $select);
 
 ?>
 
-<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
+    <style>
         body{
             background-color:#DEDAD2;
             font-family:"Lobster";
             top:50%;
             left:50%;
-            width: 400px;
+            width: 700px;
             padding:250px;
         }
     </style>
@@ -42,9 +41,9 @@ $queryselect = mysqli_query($conexao, $select);
 				<td><a href="chamausuario.php?cod=<?php echo $linha[3] ?>"><?php echo $linha[0] ?></a></td>
 			</tr>
 		<?php } ?>
-		</table><br>
-        <a href="principal.php"><input type="button" value="Sair"></a>
+		</table>
 		</form>
+		<a href="principal.php">Voltar</a>
 	</center>
 </body>
 </html>
