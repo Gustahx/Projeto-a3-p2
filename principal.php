cae208e0aefb6a63d61b446f4986bdae015e576a
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-11-24 12:01:15 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -8,29 +8,27 @@ include('validarlogin.php');
 $nivel = $_SESSION['nivel'];
 
 ?>
-
 <link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" text="text/css" href="estilo.css">  
+	<link rel="stylesheet" text="text/css" href="estilo.css">  
 	<title></title>
 </head>
 <body>
 	<center>
 		<?php
 		if ($nivel < 3) { ?>
-			 <a href="addusuario.php"><input type="button" name="Adicionar Usuário" value="Adicionar Usuário"></a><br><br>
-            <?php }
+		<a href="addusuario.php">Adicionar Usuário</a><br>
+		<?php }
 			if ($nivel == 1) { ?>
-                <a href="mudaracesso.php"><input type="button" name="Mudar Acesso" value="Mudar Acesso"></a><br><br>
-                <a href="alterardados.php"><input type="button" value="Alterar Dados"></a>
-                <br><br>
-		<?php } ?>
-        <a href="logout.php"><input type="button" name="Sair" value="Sair"></a>
+			<a href="mudaracesso.php">Mudar Acesso</a><br>
+		<?php } ?> | 
+		<a href="alterardados.php">Alterar Dados</a>
+		<br>
+		<a href="logout.php">Sair</a>
 	</center>
 </body>
 </html>
